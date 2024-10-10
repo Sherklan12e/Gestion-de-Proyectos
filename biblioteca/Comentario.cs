8c7d5bf49f9287b48a057e815461888f50adeab2
Sherklan12e <97853791+Sherklan12e@users.noreply.github.com>
2024-10-10 10:40:42 -0300

    changes

--- biblioteca/Comentario.cs
+++ biblioteca/Comentario.cs
@@ -1,7 +1,8 @@
 namespace biblioteca;
 
 public class Comentario{
-    public int id { get; set; }
+    public int Id { get; set; }
+    public Ticket ticket { get; set; }
     public Usuario usuario  {get; set; }
     public string contenido {get; set; }
     public DateTime Fecha {get; set; }
--- biblioteca/Ticket.cs
+++ biblioteca/Ticket.cs
@@ -1,13 +1,84 @@
 namespace biblioteca;
 
-public class Ticket{
+public enum Tickets
+{
+    Abierto,
+    EnProceso,
+    Testing,
+    Finalizado
+}
+
+public class Ticket
+{
     public int Id { get; set; }
     public Usuario usuario { get; set; }
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
-    public string Estado { get; set; } = "Abierto";
+    public Tickets Estado { get; set; } = Tickets.Abierto;
     public DateTime FechaInicio { get; set; }
     public DateTime? FechaFini { get; set; }
-    public List<Comentario> comentarios{ get; set; }=new List<Comentario>();
+    public List<Comentario> Actividad { get; set; } = new List<Comentario>();
+
+
+    public void Iniciarproceso( )
+    {
+        if (Estado == Tickets.Abierto)
+        {
+            Estado = Tickets.EnProceso;
+        }
+        else
+        {
+            throw new Exception("El ticket no estaba abierto");
+        }
+    }
+
+    public void IniciarTesting()
+    {
+        if (Estado == Tickets.Abierto)
+        {
+            Estado = Tickets.EnProceso;
+        }
+        else
+        {
+            throw new Exception("El ticket no estaba abierto");
+        }
+    }
+    public void TerminarTesting()
+    {
+        if (Estado == Tickets.Abierto)
+        {
+            Estado = Tickets.EnProceso;
+        }
+        else
+        {
+            throw new Exception("El ticket no estaba abierto");
+        }
+    }
+    public void ModificarInformacion(string? nombre = null, string? descripcion = null, Usuario? usuarioAsignado = null)
+        {
+            if (nombre != null)
+                Nombre = nombre;
+
+            if (descripcion != null)
+                Descripcion = descripcion;
+
+            if (usuarioAsignado != null)
+                usuario = usuarioAsignado;
+        }
+
+    public void BorrarTicket()
+    {
+        Console.WriteLine("eliminado");
+    }
+
+    public void AgregarComentario(Usuario unnusuario, string contenido, Ticket unticket)
+    {
+        Actividad.Add(new Comentario(){
+            ticket =  unticket,
+            usuario = unnusuario,
+            contenido = contenido,
+            Fecha =  DateTime.Now
+        });
+    }
 
 }
